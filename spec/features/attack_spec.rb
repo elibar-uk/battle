9042b9a46d65f50049c25e1b9e47a9caf2d4cff5
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-27 15:49:29 +0100

    adds default value and attack get_damage methods

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature "attack" do
     click_button "Attack"
     expect(page).to have_content('Bob attacked Mike')
   end
+  scenario "player 2's HP reduces by 10 after attack" do
+    sign_in_and_play
+    click_button "Attack"
+    click_link 'OK'
+    expect(page).to_not have_content 'Mike: 60HP'
+    expect(page).to have_content 'Mike: 50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,28 @@
 require 'player'
 
 describe Player do
-  subject(:bob) {described_class.new('Bob')}
+  subject(:bob) { Player.new('Bob') }
+  subject(:mike) { Player.new('Mike') }
 
+describe '#name' do
   it 'returns a name' do
     expect(bob.name).to eq 'Bob'
   end
+end
+describe 'hit_points' do
+  it 'returns the hit points' do
+    expect(bob.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+  end
+end
+describe '#attack' do
+  it 'damages the player' do
+    expect(mike).to receive(:get_damage)
+    bob.attack(mike)
+  end
+end
+describe '#get_damage' do
+  it 'reduces the player hit points' do
+    expect { mike.get_damage}.to change {mike.hit_points}.by(-10)
+  end
+end
 end
